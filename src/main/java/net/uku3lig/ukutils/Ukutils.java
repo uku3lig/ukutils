c579370850374b5a31ad1386dda6bf4ed14f407e
Uku3lig <thelololomail@gmail.com>
2022-01-05 20:15:52 +0100

    added quoted argument parsing

--- src/main/java/net/uku3lig/ukutils/Ukutils.java
+++ src/main/java/net/uku3lig/ukutils/Ukutils.java
@@ -5,9 +5,15 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
 public final class Ukutils extends JavaPlugin {
     private static final String UKUTILS = ChatColor.GRAY + "[" + ChatColor.DARK_AQUA +
             "ukutils" + ChatColor.GRAY + "]" + ChatColor.RESET;
+    private static final Pattern QUOTE_PATTERN = Pattern.compile("[\"'][^\"']++[\"']|[^\\s]+");
 
     @Override
     public void onEnable() {
@@ -22,6 +28,14 @@ public final class Ukutils extends JavaPlugin {
     }
 
     public static void sendMessage(CommandSender p, String message) {
-        p.sendMessage(UKUTILS + " " + message);
+        p.sendMessage(UKUTILS_PREFIX + " " + message);
+    }
+
+    public static List<String> parseArgs(String... args) {
+        String unified = String.join(" ", args);
+        return QUOTE_PATTERN.matcher(unified).results()
+                .map(MatchResult::group)
+                .map(s -> s.replace("\"", ""))
+                .toList();
     }
 }
