3036f6ac5c6007ca6cb0e9ad87eeb9c862fd109c
Uku3lig <thelololomail@gmail.com>
2022-01-05 20:18:42 +0100

    add better title length checking

--- src/main/java/net/uku3lig/ukutils/TitleCommand.java
+++ src/main/java/net/uku3lig/ukutils/TitleCommand.java
@@ -7,25 +7,30 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public class TitleCommand implements CommandExecutor {
     private static final String TITLE_FORMAT = "lp user %s meta setprefix 1000 \"&r[%s&r]\"";
     private static final String REMOVE_TITLE_FORMAT = "lp user %s meta removeprefix 1000";
+    private static final String COLOR_PATTERN = "&?#[a-f0-9]{6}|&[a-f0-9]";
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player player && args.length < 2) {
-            if (args.length == 0) resetTitle(sender, player);
+        List<String> arguments = Ukutils.parseArgs(args);
+        System.out.println(arguments);
+        if (sender instanceof Player player && arguments.size() < 2) {
+            if (arguments.isEmpty()) resetTitle(sender, player);
             else {
-                Player target = Bukkit.getPlayerExact(args[0]);
+                Player target = Bukkit.getPlayerExact(arguments.get(0));
                 if (target != null && sender.hasPermission("ukutils.title.others")) resetTitle(sender, target);
-                else changeTitle(sender, player, args[0]);
+                else changeTitle(sender, player, arguments.get(0));
             }
         } else {
-            Player target = Bukkit.getPlayerExact(args[0]);
+            Player target = Bukkit.getPlayerExact(arguments.get(0));
             if (target == null) Ukutils.sendMessage(sender, ChatColor.RED + "Error: cannot find that player");
             else if (!sender.hasPermission("ukutils.title.others"))
                 Ukutils.sendMessage(sender, ChatColor.RED + "Error: cannot change this person's title");
-            else changeTitle(sender, target, args[1]);
+            else changeTitle(sender, target, arguments.get(1));
         }
         return true;
     }
@@ -37,9 +42,14 @@ public class TitleCommand implements CommandExecutor {
     }
 
     private void changeTitle(CommandSender sender, Player target, String title) {
-        int length = Math.min(title.length(), target.hasPermission("ukutils.title.longer") ? 20 : 15);
-        String c = TITLE_FORMAT.formatted(target.getName(), title.substring(0, length));
-        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), c);
-        Ukutils.sendMessage(sender, "Successfully changed title.");
+        int maxLength = Math.min(title.length(), target.hasPermission("ukutils.title.longer") ? 20 : 15);
+        if (title.replaceAll(COLOR_PATTERN, "").length() > maxLength)
+            Ukutils.sendMessage(sender, ChatColor.RED + "Error: the title cannot be longer than " +
+                    maxLength + " characters! (excluding color codes)");
+        else {
+            String c = TITLE_FORMAT.formatted(target.getName(), title);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), c);
+            Ukutils.sendMessage(sender, "Successfully changed title.");
+        }
     }
 }
