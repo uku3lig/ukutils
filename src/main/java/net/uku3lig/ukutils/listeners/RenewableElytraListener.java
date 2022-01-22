836be65f95a5860a8f78be41eb4ad9186df5362a
Uku3lig <thelololomail@gmail.com>
2022-01-22 12:39:12 +0100

    added RenewableElytraListener

---
+++ src/main/java/net/uku3lig/ukutils/listeners/RenewableElytraListener.java
@@ -0,0 +1,28 @@
+package net.uku3lig.ukutils.listeners;
+
+import net.uku3lig.ukutils.Ukutils;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class RenewableElytraListener implements Listener {
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (!event.getEntity().getType().equals(EntityType.ENDER_DRAGON)) return;
+        if (event.getEntity().getKiller() == null) return;
+        if (!event.getEntity().getKiller().hasPermission("ukutils.elytra")) return;
+
+        double drop = Ukutils.getInstance().getConfig().getDouble("elytra-drop-chance", 0.02);
+        if (Math.random() >= drop) return;
+
+        Player player = event.getEntity().getKiller();
+        if (!player.getInventory().addItem(new ItemStack(Material.ELYTRA)).isEmpty()) {
+            event.getEntity().getWorld().dropItemNaturally(player.getLocation(), new ItemStack(Material.ELYTRA));
+        }
+        player.sendMessage("An elytra has been added to your inventory.");
+    }
+}
